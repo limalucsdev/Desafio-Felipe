22d5f778e1fe678fdf56b9e5bdc8236192231c61
limalucsdev <lucas_rj1@hotmail.com>
2023-12-14 19:36:27 -0300

    Desafio do Felipe

--- SRC/index.js
+++ SRC/index.js
@@ -1,1 +1,20 @@
-console.log("olá");
+// Desafio Classificador de nível de Herói 
+
+// O que deve ser utilizado
+
+// Variáveis, Operadores, Laços de Repetição, Estrutura de Decisão
+// Desafio: Criar uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+// Se XP for menor do que 1000 = Ferro 
+// Se XP for entre 1001 e 2000 = Bronze
+// Se XP for entre 2001 e 5000 = Prata
+// Se XP for entre 6001 e 7000 = Ouro
+// Se XP for entre 7001 e 8000 = Platina
+// Se XP for entre 8001 e 9000 = Ascendente
+// Se XP for entre 9001 e 10000 = Imortal
+// Se XP for maior ou igual a 10001 = imortal
+
+// Saída 
+// Ao final deve se exibir uma mensagem: " O herói de nome Sirlucs está no nível de -----"
+
+let nomeDoHeroi = "Sirlucs"
+let experiência = 
